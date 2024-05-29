b434c72f1c5af59359b2d91a835a6db09fd45b56
Tautvydas Pagalys <tpagalys@gmail.com>
2024-05-29 06:54:12 +0300

    Initial commit: project created with IntelliJ Ultimate Spring Boot generator, nothing changed.

---
+++ src/main/java/lt/statas/invoicing/InvoicingSoftwareBeApplication.java
@@ -0,0 +1,13 @@
+package lt.statas.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingSoftwareBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicingSoftwareBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/statas/invoicing/InvoicingSoftwareBeApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.statas.invoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingSoftwareBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
